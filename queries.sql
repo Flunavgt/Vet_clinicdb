e332ea07dca5f3e5ca6247fd14a75e9ffc3c566b
Federico Luna <fluna1973@yahoo.com>
2022-10-17 17:21:14 -0600

    Update queries.sql
    Update on date range on query

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 SELECT * from animals WHERE name = 'Luna';
 
 SELECT * FROM animals WHERE name LIKE '%mon%';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered=TRUE AND escape_attempts <3;
 SELECT date_of_birth from animals WHERE name='Agumon' OR name='Pikachu';
 SELECT name, escape_attempts from animals WHERE  weight_kg>10.5;
