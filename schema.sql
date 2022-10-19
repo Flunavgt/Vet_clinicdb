fd675e6e37c65cbc5bd10cc719df89fc650c9243
Federico Luna <fluna1973@yahoo.com>
2022-10-19 15:07:54 -0600

    Update schema.sql
    Update to 3rd day activities

--- schema.sql
+++ schema.sql
@@ -14,3 +14,20 @@ CREATE TABLE animals (
 Alter table insert column. */
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+
+/* day 3:
+add multiple tables. */
+
+CREATE TABLE owners (id int GENERATED ALWAYS AS IDENTITY, full_name varchar(40), age int, PRIMARY KEY(id));
+CREATE TABLE species (id int GENERATED ALWAYS AS IDENTITY, name varchar(40), PRIMARY KEY(id));
+\d
+
+ALTER TABLE animals DROP COLUMN species;
+SELECT * FROM animals;
+
+ ALTER TABLE animals ADD COLUMN species_id int;
+ ALTER TABLE animals ADD COLUMN owner_id int;
+ 
+ ALTER TABLE animals ADD CONSTRAINT fk_animals FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE;
+ ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
